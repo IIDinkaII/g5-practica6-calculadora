3bf9f73143ef2e3773d649613fbf723ab962bcdb
IIDinkaII <diana.lopez03@epn.edu.ec>
2021-01-14 19:13:36 -0500

    Añadiendo la clase de ejecución de la calculadora y el archivo JAR de esta.

---
+++ src/main/java/ec/edu/epn/git/calculadora/CalculadoraExecute.java
@@ -0,0 +1,16 @@
+package ec.edu.epn.git.calculadora;
+
+public class CalculadoraExecute {
+    public static void main(String[] args){
+        System.out.println("----Ejecución Calculadora----");
+        Calculadora cal = new Calculadora();
+        int a = 6;
+        int b = 3;
+        System.out.println("Los números son: " + a +" y "+ b);
+        int suma = cal.addition(a,b);
+        System.out.println("Suma: " + suma);
+        int resta = cal.subtraction(a,b);
+        System.out.println("Resta: " + resta);
+
+    }
+}
